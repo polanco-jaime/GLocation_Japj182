e32f7fb36d6cf36ea08dab62f84f8c8612d86fbf
JAIME ANDRES POLANCO JIMENEZ <49122673+JAPJ182@users.noreply.github.com>
2021-02-24 14:01:17 -0500

    Update modelo logistico.R
    Eliminación hacia atrás utilizando solamente p-valores:
    
    import statsmodels.formula.api as sm
    def backwardElimination(x, sl):    
        numVars = len(x[0])    
        for i in range(0, numVars):        
            regressor_OLS = sm.OLS(y, x.tolist()).fit()        
            maxVar = max(regressor_OLS.pvalues).astype(float)        
            if maxVar > sl:            
                for j in range(0, numVars - i):                
                    if (regressor_OLS.pvalues[j].astype(float) == maxVar):                    
                        x = np.delete(x, j, 1)    
        regressor_OLS.summary()    
        return x 
     
    SL = 0.05
    X_opt = X[:, [0, 1, 2, 3, 4, 5]]
    X_Modeled = backwardElimination(X_opt, SL)
    Eliminación hacia atrás utilizando  p-valores y el valor de  R Cuadrado Ajustado:
    
    import statsmodels.formula.api as sm
    def backwardElimination(x, SL):    
        numVars = len(x[0])    
        temp = np.zeros((50,6)).astype(int)    
        for i in range(0, numVars):        
            regressor_OLS = sm.OLS(y, x.tolist()).fit()        
            maxVar = max(regressor_OLS.pvalues).astype(float)        
            adjR_before = regressor_OLS.rsquared_adj.astype(float)        
            if maxVar > SL:            
                for j in range(0, numVars - i):                
                    if (regressor_OLS.pvalues[j].astype(float) == maxVar):                    
                        temp[:,j] = x[:, j]                    
                        x = np.delete(x, j, 1)                    
                        tmp_regressor = sm.OLS(y, x.tolist()).fit()                    
                        adjR_after = tmp_regressor.rsquared_adj.astype(float)                    
                        if (adjR_before >= adjR_after):                        
                            x_rollback = np.hstack((x, temp[:,[0,j]]))                        
                            x_rollback = np.delete(x_rollback, j, 1)     
                            print (regressor_OLS.summary())                        
                            return x_rollback                    
                        else:                        
                            continue    
        regressor_OLS.summary()    
        return x 
     
    SL = 0.05
    X_opt = X[:, [0, 1, 2, 3, 4, 5]]
    X_Modeled = backwardElimination(X_opt, SL)

--- modelo logistico.R
+++ modelo logistico.R
@@ -9,34 +9,23 @@ library(dplyr)
 # Set your query
 
 sql <- "
-SELECT
---CIENCIAS_SOCIALES AREA,
---CIENCIAS_NATURALES AREA,
---CIENCIAS_AGRICOLAS AREA,
---INGENIERIA_Y_TECNOLOGIA AREA,
---CIENCIAS_MEDICAS_Y_DE_LA_SALUD AREA,
-
---SOCIAL,CULTURA,SOCIEDAD,CONFLICTO,ECONOMICO,ECONOMIA,POLITICA,DERECHO,FORMACION,EDUCACION,ECONOM,SOCIALES,ESTUDIOS,COMUNIDAD,PAZ,JURIDICO,PEDAGOGICO,GLOBALIZACION,INDEXADAS,DEMOCRACIA,RELIGACION,RELACIONES_INTERNACIONALES,MERCADEO,FINANZA,SOCIOECONOMICO,CAPITALISMO,LEYES,TRABAJO_SOCIAL,SOCIOLOGIA,RELIGION,VIOLENCIA,CORRUPCION,CRIMEN,POBREZA,CURRICULO,INTERCULTURALIDAD,SOCIO,
---MATERIALES,CONSERVACION,QUIMICA,BIOLOGIA,FISICA,ESPECIES,BIODIVERSIDAD,ECOSISTEMAS,AMBIENTE,BIOLOGICA,MICROBIOLOGIA,FAUNA,ACUATICOS,OPTICA,LASER,GEOLOGIA,BIOTECNOLOGIA,BIOINGENIERIA,SOSTENIBLE,BIOCALORIMETRIA,FORESTAL,MATEMATICAS,RECURSOS_NATURALES,MEDIO_AMBIENTE,TERMICAS,CAMBIO_CLIMATICO,FISICOQUIMICA,NEOTROPICALES,MORFOLOGIA OCEANOLOGIA,COSMOLOGIA,ASTROFISICA,ASTRONOMIA,ECOLOGIA,CONTAMINACION,METALURGIA,MATEMATICA,MICROALGAS,IMPACTO_AMBIENTAL,VIDA_SILVESTRE,RENOVABLES,ECUACIONES_DIFERENCIALES,CIENCIAS_BASICAS,
-----AGROPECUARI,ANIMAL,PLANTAS,PLAGAS,AGRICULTURA,TROPICALES,CULTIVO,SUELOS,CAFE,AGROINDUSTRIAL,VETERINARIA,AGRICOLA,AGROINDUSTRIALES,TROPICAL,AGROECOSISTEMAS,PALMA,AGRARIAS,VEGETALES,ACUICULTURA,AGRONOMICAS,CRIOLLO,GANADER,ARROZ,CULTIV,AGRONEGOCIO,PECES,CONURBANO,AGROFORESTAL,GIES,CARIBE,SALUD_ANIMAL,GIDOCA,BIOTOX,GINPAS,GRITOX,GIPSA,APICULTURA,AGRARIO,CIENCIAS_ANIMALES,PECUARIA,BANANO,AGUACATE,PAPA,CANA_DE_AZUCAR,VACUNO,HIDROLOGIA,HIDRO,HIDRICO,
---DISENO, TECNOLOGICO,TECNOLOGIA,PROGRAMAS,TECNOLOGICA,HERRAMIENTAS,APLICACIONES,FOMENTAR,SOFTWARE,TELECOMUNICACIONES,PROGRAMA,AUTOMATIZACION,LABORATORIO,INGENIERIA,ENERGETICA,BIG_DATA,MACHINE_LEARNING,ARTIFICIAL_INTELLIGENCE,DEEP,INGENIERIA_AMBIENTAL,AUTOMATICA,INFRAESTRUCTU,ROBOTICA,INFORMATICA,ENGINEERING,CIENCIAS_TERMICAS,NANOESTRUCTURA,CIENCIAINGENIERIA,
---SALUD,MEDICINA,CLINICA,VIDA,EPIDEMIOLOGIA,ENFERMEDAD,PACIENTE,ENFERMERIA,REHABILITACION,INFECCIOS,CANCER,DISCAPACIDAD,MEDICAMENTO,INMUNOLOGIA,CLINICO,PATOLOGIA,BIOMEDICA,MEDICA,TERAPEUTICA,CIRUGIA,EPIDEMIOLOGICO,ODONTOLOGIA,HOSPITAL,FARMACEUTICA,VIRUS,SALUD_NFERMEDAD,CARDIOVASCULAR,FISIOTERAPIA,ARTRITIS,ESCLEOLORISIS,ANTIBIOTICO,NEUMONIA,MUSCULO,QUIRURGICA,TUMOR,METABOLISMO,GINECOLO
-
---from 
---  `analitica-302215.GRUPOS_AUTORES.traini1` 
  
-SELECT 
-CIENCIAS_SOCIALES AREA,
-JURIDICO, PEDAGOGICO, GLOBALIZACION, INDEXADAS,  DEMOCRACIA, RELIGACION, RELACIONES_INTERNACIONALES, MERCADEO, FINANZA, SOCIOECONOMICO,  CAPITALISMO, LEYES, TRABAJO_SOCIAL,  SOCIOLOGIA, RELIGION,VIOLENCIA, CORRUPCION ,SOCIALES, ESTUDIOS, EDUCACION,  ECONOMIA, POLITICA, DERECHO, SOCIAL, CULTURA , CONFLICTO,  CRIMEN, POBREZA, CURRICULO, INTERCULTURALIDAD,
+SELECT
+CIENCIAS_AGRICOLAS,
+AGROPECUARI, ANIMAL,  PLAGAS, AGRICULTURA,CULTIVO, SUELOS, CAFE, AGROINDUSTRIAL, VETERINARIA, AGRICOLA, TROPICAL, AGROECOSISTEMAS, PALMA, AGRARIAS, VEGETALES, ACUICULTURA, AGRONOMICAS, CRIOLLO,  
+GANADER, ARROZ,  CULTIV, AGRONEGOCIO, PECES, CONURBANO, AGROFORESTAL, GIES, CARIBE, SALUD_ANIMAL,  GIDOCA, BIOTOX, GINPAS, GRITOX, GIPSA, APICULTURA,AGRARIO, CIENCIAS_ANIMALES, PECUARIA,BANANO, AGUACATE, 
+PAPA, CANA_DE_AZUCAR, BIOTECNOLOGIA,  VACUNO, 
+
 from 
   `analitica-302215.GRUPOS_AUTORES.traini1`
 
  "
-
-
+variable_dependiente = 'CIENCIAS_AGRICOLAS'
+###########################  read query and clean  ##################################################
 
 # Run the query and store the data in a dataframe
 df <- query_exec(sql, projectid, use_legacy_sql = FALSE, max_pages = Inf)
+
 #library(dplyr)
 n = ncol(df) 
 for (i in 1:n) { 
@@ -52,13 +41,13 @@ for (i in 1:n) {
   mylist <- rbind(mylist, listtmp)
   mylist <- subset(mylist, str_detect(paste0(mylist[[1]],' ' ,mylist[[2]] ), "FALSE") ==T) 
 }
-mylist[] <- lapply(mylist[], gsub, pattern=' FALSE', replacement='')
+
 df = df %>% dplyr::select(-(mylist[[1]]))
 
 ################### subseting in train and test
 
 set.seed(4123)
-split = caTools::sample.split(df$AREA, SplitRatio = 0.8)
+split = caTools::sample.split(df[variable_dependiente], SplitRatio = 0.8)
 training_set = subset(df, split == TRUE)
 testing_set = subset(df, split == FALSE)
 
@@ -68,13 +57,48 @@ testing_set = subset(df, split == FALSE)
 
 
 ####################
+x = df
 
-backwardElimination <- function(x, sl) {
-  vardep <- x %>% dplyr::select(AREA)
-  varind <- x %>% dplyr::select(-AREA)
+backwardElimination_aic <- function(x, sl, variable_dependiente){
+  vardep <- x %>% dplyr::select(variable_dependiente)
+  colnames(vardep)[1] = 'Y'
+  varind <- x %>% dplyr::select(-variable_dependiente)
+  numVars = ncol(varind)
+  temp = matrix(data = 0, ncol = ncol(varind), nrow = nrow(varind))
+  temp <- as.data.frame(temp)
+  colnames(temp) <- colnames(varind)
+  for (i in c(1:numVars)){
+    regressor = glm(data = cbind(vardep, varind), formula = Y ~ ., family = "binomial") 
+    maxVar = max(coef(summary(regressor))[c(2:numVars),4])
+    adjR_before = summary(regressor)$aic
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), 4] == maxVar)
+      temp[, j] = varind[, j]
+      varind = varind[, -j]
+      tmp_regressor = glm(data = cbind(vardep, varind), formula = Y ~ ., family = "binomial") 
+      adjR_after = summary(tmp_regressor)$aic
+      if (adjR_before >= adjR_after){
+        x_rollback <- cbind(varind = varind[,], temp[,j])
+        colnames(x_rollback) <- c(names(varind), names(temp)[j])
+        varind <-as.data.frame(x_rollback)
+      }
+      else{
+        numVars = numVars - 1
+      }
+    }
+  }
+  return(summary(regressor))
+} 
+
+distinct_(data.frame('unicos' = c(names(varind), names(temp)[j])))
+backwardElimination_aic(df, sl, variable_dependiente)
+backwardElimination <- function(x, sl, variable_dependiente) {
+  vardep <- x %>% dplyr::select(variable_dependiente)
+  colnames(vardep)[1] = 'Y'
+  varind <- x %>% dplyr::select(-variable_dependiente)
   numVars = ncol(x)
   for (i in c(1:numVars)){
-    regressor = glm(data = cbind(vardep, varind), formula = AREA ~ ., family = "binomial") 
+    regressor = glm(data = cbind(vardep, varind), formula = Y ~ ., family = "binomial") 
     maxVar = max(coef(summary(regressor))[c(2:numVars), 4])
     if (maxVar > sl){
       j = which(coef(summary(regressor))[c(2:numVars), 4] == maxVar)
@@ -87,17 +111,19 @@ backwardElimination <- function(x, sl) {
   #return(data = cbind(vardep, varind))
 }
 
-####################
+################################################
 #library(MASS)
 #modback <- stepAIC(modelo, trace=TRUE, direction="backward")
 # Ajustar el clasificador con el conjunto de entrenamiento.
 # Crear el modelo de clasificacion
-sl = 0.05
-backwardElimination(df, sl)
-modelo = glm(data=training_set, AREA ~ ., family = "binomial") 
+sl = 0.1
+backwardElimination(df, sl, variable_dependiente)
+backwardElimination_aic(df, sl, variable_dependiente)
+#####################################################
+modelo = glm(data=training_set, CIENCIAS_AGRICOLAS ~ ., family = "binomial") 
 summary(modelo)
 
-coef(summary(regressor))[ , 4]
+
 # Prediccion de los resultados con el conjunto de testing
 prob_pred = predict(modelo, type = 'response', testing_set)
 
