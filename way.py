ddf703fa33f1cf59ed8b5236ca8050e394dfb7d8
JAIME ANDRES POLANCO JIMENEZ <49122673+JAPJ182@users.noreply.github.com>
2022-09-20 17:58:40 -0500

    Update way.py

--- way.py
+++ way.py
@@ -1,7 +1,13 @@
 import os
+import sys, os
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+    
 url = '/funciones'
 url = url.split('funciones')[0] + 'funciones/gcpdefunctions'
 url = url.replace('funciones','') + '/main/other_function/'
 os.environ['location'] = 'https://raw.githubusercontent.com/'
 os.environ['execute_'] =  'JAPJ182/free_data_downloader.py'
 path = os.environ['location']+os.environ['execute_'].split('/')[0]    +url.replace('//','/')+os.environ['execute_'].split('/')[1]   
+ 
